a6f34ab552991a709a370e610b026a20a597e4c6
Pat McNeely <pmcneely@gwu.edu>
2024-06-07 11:09:57 -0400

    Commit original 'dockerrun' script for launching the analysis

---
+++ example-files/dockerrun.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#
+# Execute using docker container
+#
+
+IMAGE="glyomics/glycosight:1.1.0"
+
+USER="`id -u`:`id -g`"
+CUR=`pwd`
+CUR=`readlink -f $CUR`
+
+if [ "$1" = "" ]; then
+  echo "Usage: run.sh ( msconvert | msgfplus | nlinkedsites ) [ arguments ]" 1>&2
+  exit 1
+fi
+
+case "$1" in 
+  msconvert | msgfplus | nlinkedsites) ;;
+  *) echo "Usage: run.sh ( msconvert | msgfplus | nlinkedsites ) [ arguments ]" 1>&2; exit 1;;
+esac
+
+if [ "$1" = "msgfplus" -o "$1" = "nlinkedsites" ]; then
+  SCRIPT="$1"
+  shift
+  docker run -u $USER -v $CUR:/data/:Z --rm "$IMAGE" /GlycoSight/bin/$SCRIPT.sh "$@"
+else
+  shift
+  docker run -v $CUR:/data/:Z --rm chambm/pwiz-skyline-i-agree-to-the-vendor-licenses:latest wine msconvert "$@"
+fi
+
+

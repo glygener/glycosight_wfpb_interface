5bebc17064372a0c623b26909b3ed7e613b1ef56
Pat McNeely <pmcneely@gwu.edu>
2024-06-07 11:21:25 -0400

    Add interface API endpoints to support WFPB access

--- glycosight_interface.py
+++ glycosight_interface.py
@@ -47,6 +47,9 @@ SHIM = (
 ANALYSIS_URL = ANALYSIS_BACKEND + ":{}"
 
 
+############################### HELPERS ############################
+
+
 class LockManager:
     def __init__(self):
         self.lockedfiles = {}
@@ -76,6 +79,66 @@ class LockManager:
 lock_manager = LockManager()
 
 
+def run_analysis():
+
+    session_timer = 20
+    refresh_interval = 0.5
+
+    counter = 0
+    start = time.time()
+    app.logger.debug("...Analysis launched")
+
+    # Block response until lock is acquired
+    lockname = lock_manager.acquire_lock()
+    while not lockname:
+        time.sleep(1)
+        app.logger.debug("---> Did not get a lock. Trying again ...")
+        lockname = lock_manager.acquire_lock()
+        counter += 1
+        if counter > session_timer:
+            app.logger.debug("Session timeout error")
+            return {"error": "No compute available, try again later"}
+
+    # Launch analysis in Flask docker
+
+    analysis_target = (
+        ANALYSIS_URL.format(
+            ANALYSIS_PORT_BASE + int(lockname.split(".")[0].lstrip("file"))
+        )
+        + "/perform-analysis"
+    )
+    app.logger.debug(f"Targeting analysis on port {analysis_target}")
+
+    # Get response and return
+    if SHIM:
+        app.logger.debug("Sleeping 10 seconds while pretending to work")
+        time.sleep(10)
+        lock_manager.release_lock(lockname)
+        return {"results": "complete"}
+    app.logger.debug("Sending analysis request downstream")
+    try:
+        response = requests.get(analysis_target)
+        if response.status_code != 200:
+            lock_manager.release_lock(lockname)
+            return {"error": response.status_code}
+        lock_manager.release_lock(lockname)
+        return json.loads((response.content.decode("utf-8")))
+    except:
+        lock_manager.release_lock(lockname)
+        raise
+
+
+DUMMY_DATA = {
+    "UniProtAcc": ["This", "is", "a", "test"],
+    "AAPosition": [1, 2, 3],
+    "SpectralCount": [4, 5, 6],
+    "DistinctPeptideCount": [7, 8, 9],
+    "Peptides": ["sample", "peptide", "strings"],
+}
+
+####################################################################
+
+
 @app.route("/ping", methods=["GET"])
 def ping():
     try:
@@ -86,9 +149,47 @@ def ping():
         return Response(f"ERROR: {e}")
 
 
-@app.route("/api/upload", methods=["POST"])
+@app.route("/api/upload-and-analyze", methods=["POST"])
 @cross_origin("Access-Control-Allow-Origin")
-def upload():
+def upload_and_analyze():
+    target = request.args.get("q", None)
+    file_name = request.args.get("n", None)
+    app.logger.debug(f"Got request! Target URL was {target}. Filename was {file_name}")
+    if target.startswith("drs"):
+        locator = target.rsplit("/")[-1]
+        app.logger.debug(f"Found URI locator {locator}")
+        resourceURI = (
+            f"http://localhost:3000/ga4gh/drs/v1/objects/{locator}/access/https/data"
+        )
+
+        response = requests.request("GET", resourceURI)
+        if response.status_code != 200:
+            # Error handling?
+            app.logger.debug("===> ERROR NOTED <===")
+            return jsonify(DUMMY_DATA)
+
+    else:
+        raise Exception("Engaging self destruct ... NOW")
+    # Write data to disk
+    start = time.time()
+    data = response.content
+    file_hash = md5(data).hexdigest()
+    duration = time.time() - start
+    app.logger.debug(
+        f"\n====== DATA\n\t-- Length {len(data)}\n\t-- Hash: {file_hash}\n\t-- Time needed: {duration * 1000:.1f} ms\n========="
+    )
+    with open(os.path.join(WORK_DIR, file_name), "wb") as fp:
+        fp.write(data)
+    fp.close()
+
+    # Launch analysis
+    result = run_analysis()
+    return jsonify(result)
+
+
+@app.route("/api/standalone-upload", methods=["POST"])
+@cross_origin("Access-Control-Allow-Origin")
+def standalone_upload():
     target = request.args.get("q", None)
     file_name = request.args.get("n", None)
     app.logger.debug(f"Got request! Target was {target}, filename was {file_name}")
@@ -105,46 +206,18 @@ def upload():
     return jsonify({"results": "complete"})
 
 
-@app.route("/api/analyze")
+@app.route("/api/test-access", methods=["POST"])
 @cross_origin("Access-Control-Allow-Origin")
-def analyze():
+def test_access():
+    target = request.args.get("q", None)
+    app.logger.debug(f"Got TEST request! Target URL: {target}")
+    return jsonify(DUMMY_DATA)
 
-    session_timer = 20
-    refresh_interval = 0.5
 
-    counter = 0
-    start = time.time()
-    app.logger.debug("...Analysis launched")
+@app.route("/api/standalone-analyze")
+@cross_origin("Access-Control-Allow-Origin")
+def standalone_analyze():
 
-    # Block response until lock is acquired
-    lockname = lock_manager.acquire_lock()
-    while not lockname:
-        time.sleep(1)
-        app.logger.debug("---> Did not get a lock. Trying again ...")
-        lockname = lock_manager.acquire_lock()
-    # Launch analysis in Flask docker
+    result = run_analysis()
 
-    analysis_target = (
-        ANALYSIS_URL.format(
-            ANALYSIS_PORT_BASE + int(lockname.split(".")[0].lstrip("file"))
-        )
-        + "/perform-analysis"
-    )
-    app.logger.debug(f"Targeting analysis on port {analysis_target}")
-
-    # Get response and return
-    if SHIM:
-        app.logger.debug("Sleeping 10 seconds while pretending to work")
-        time.sleep(10)
-        lock_manager.release_lock(lockname)
-        return jsonify({"results": "complete"})
-    app.logger.debug("Sending analysis request downstream")
-    try:
-        response = requests.get(analysis_target)
-        if response.status_code != 200:
-            lock_manager.release_lock(lockname)
-            return jsonify({"errror": response.status_code})
-    except:
-        raise
-    lock_manager.release_lock(lockname)
-    return jsonify(json.loads((response.content.decode("utf-8"))))
+    return jsonify(result)

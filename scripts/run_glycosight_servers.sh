6de267803d6ca823e3706cf2586a90bc4fa3325f
Pat McNeely <pmcneely@gwu.edu>
2024-07-11 12:31:45 -0400

    Fix race condition in generating locks prior to starting servers

--- scripts/run_glycosight_servers.sh
+++ scripts/run_glycosight_servers.sh
@@ -5,13 +5,9 @@ source shutdown_servers.sh
 pushd .. 2>&1 > /dev/null
 
 rm locks/file*.lock
+rm -rf tmp/*
 
-echo Starting interface server ...
-docker run --rm -d --network wfpb --name glycosight-interface -p 5050:5000 \
-    --mount src=$(pwd)/tmp/,target=/flask/tmp,type=bind \
-    --mount src=$(pwd)/locks/,target=/flask/locks,type=bind \
-    glycosight-interface flask --app glycosight_interface.py run --port=5000 --host=0.0.0.0
-
+# Create locks and destination directories before starting the servers
 for i in $(seq 1 $SERVER_COUNT); do
     if [[ ! -d "tmp/$i" ]]; then
         echo Did not find tmp folder $i - creating it
@@ -25,6 +21,15 @@ for i in $(seq 1 $SERVER_COUNT); do
     else
         echo Found lock file $i, proceeding
     fi
+done
+
+echo Starting interface server ...
+docker run --rm -d --network wfpb --name glycosight-interface -p 5050:5000 \
+    --mount src=$(pwd)/tmp/,target=/flask/tmp,type=bind \
+    --mount src=$(pwd)/locks/,target=/flask/locks,type=bind \
+    glycosight-interface flask --app glycosight_interface.py run --port=5000 --host=0.0.0.0
+
+for i in $(seq 1 $SERVER_COUNT); do
     echo Starting backend server: $i
     docker run --rm -d --network wfpb --name glycosight-backend-$i \
         --mount src=$(pwd)/tmp/,target=/flask/tmp,type=bind \
